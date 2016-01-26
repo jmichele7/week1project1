2eb391b672ac3713d422df4e4e6021b2fd57a588
Michele Pruitt <jmichelepruitt@gmail.com>
2016-01-25 17:10:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
 //Week 1 - Project 1
 
 // Log all answers to the console and write them to document
-// =======================================================
 
 //1. Log your name to the console (as a string).
+console.log("Michele")
 
 //2. Declare a variable called name, log it to the console.
+var name = "Michele";
+console.log(name)
 
 //3. Declare two variables with number values. Add, subtract, multiply and divide them.
+var x = 5;
+var y = 10;
+console.log(x + y); (x - y); (x * y); (x / y); 
 
 //4. Declare two variables with string values. Create a third variable that concatenates them.
+var myCity = "Denver";
+var myState = "Colorado";
+var myAddress = myCity + myState
 
 //5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
+var aa = '5';
+var bb = '3';
+aa + bb;
+aa - bb;
+aa * bb;
+aa / bb;
 
 //6. Create a prompt that asks a user to pick a number. Double the answer.
+prompt("Pick a Number!")
 
 //7. Create a custom alert.
+alert("I am excited for JS Class Tonight!");
 
 //8. Declare three variables called length, width and height. Calculate area and volume.
+var length = 3
+var width = 5
+var height = 7
 
 //9. Ask for a user's name, age and city. In a single command display/log the sentence:
 // "My name is...I'm....years old, and I live in....."
 
+var yourName
+var yourAge
+var yourCity
+
+prompt("What is your name, age and city?")
+console.log("My name is ... I'm ... years old, and I live in ...")
+
 //10. Create an alert that uses the response from a prompt.
+alert()
